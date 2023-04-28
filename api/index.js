fc31a7f2b634d395c484102f1195fb251b6d8aed
Ianduha13 <ianduhamelhayes@gmail.com>
2023-04-27 23:46:10 -0300

    Deploy fix

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ const express = require("express")
 const cors = require("cors")
 require("dotenv").config()
 const { graphqlHTTP } = require("express-graphql")
-const schema = require("../schema/schema")
-const connectDB = require("../config/db")
+const schema = require("./schema/schema")
+const connectDB = require("./config/db")
 const port = process.env.PORT || 3000
 
 const app = express()
